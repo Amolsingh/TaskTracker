9395da9ccc1798d482ae2b8ef413f3d0e36b9331
AMOL SINGH <amolsingh@deloitte.com>
2018-05-20 16:03:18 +0530

    Amol commit

--- TaskTracker/src/main/java/com/myTeam/myApp/HomeController.java
+++ TaskTracker/src/main/java/com/myTeam/myApp/HomeController.java
@@ -25,7 +25,7 @@ public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
+		logger.info("The great shivam", locale);
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
